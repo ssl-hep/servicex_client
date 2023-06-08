7f5553e720a1b1c565e0d2bfb5f0f70c37c09663
Ben Galewsky <ben@peartreestudio.net>
2023-06-08 10:21:04 -0500

    Finish up func_adl refactor

--- servicex_client/dataset.py
+++ servicex_client/dataset.py
@@ -25,6 +25,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import abc
 import ast
 import asyncio
 import copy
@@ -57,8 +58,6 @@ from servicex_client.servicex_adapter import ServiceXAdapter
 
 
 class Dataset(ABC):
-    # These are methods that are translated locally
-    _execute_locally = ["ResultPandasDF", "ResultAwkwardArray"]
 
     def __init__(
         self,
@@ -95,28 +94,9 @@ class Dataset(ABC):
         self.servicex_polling_interval = servicex_polling_interval
         self.minio_polling_interval = minio_polling_interval
 
-    def clone_with_new_ast(self, new_ast: ast.AST, new_type: typing.Any):
-        """
-        Override the method from ObjectStream - We need to be careful because the query
-        cache is a tinyDB database that holds an open file pointer. We are not allowed
-        to clone an open file handle, so for this property we will copy by reference
-        and share it between the clones. Turns out ast class is also picky about copies,
-        so we set that explicitly.
-        :param new_ast:
-        :param new_type:
-        :return:
-        """
-        clone = copy.copy(self)
-        for attr, value in vars(self).items():
-            if type(value) == QueryCache:
-                setattr(clone, attr, value)
-            elif attr == "_q_ast":
-                setattr(clone, attr, new_ast)
-            else:
-                setattr(clone, attr, copy.deepcopy(value))
-
-        clone._item_type = new_type
-        return clone
+    @abc.abstractmethod
+    def generate_selection_string(self) -> str:
+        pass
 
     @property
     def transform_request(self):
@@ -128,7 +108,7 @@ class Dataset(ABC):
             codegen=self.codegen,
             result_destination=ResultDestination.object_store,
             result_format=self.result_format,
-            selection=self.generate_qastle(self.query_ast)
+            selection=self.generate_selection_string()
         )
         # Transfer the DID into the transform request
         self.dataset_identifier.populate_transform_request(sx_request)
@@ -333,106 +313,3 @@ class Dataset(ABC):
     async def as_signed_urls(self):
         return await self.submit_and_download(signed_urls_only=True)
 
-    def generate_qastle(self, a: ast.AST) -> str:
-        """Generate the qastle from the ast of the query.
-
-        1. The top level function is already marked as being "ok"
-        1. If the top level function is something we have to process locally,
-           then we strip it off.
-
-        Args:
-            a (ast.AST): The complete AST of the request.
-
-        Returns:
-            str: Qastle that should be sent to servicex
-        """
-        top_function = cast(ast.Name, a.func).id
-        source = a
-        if top_function in self._execute_locally:
-            # Request the default type here
-            default_format = self._ds.first_supported_datatype(["parquet", "root-file"])
-            assert default_format is not None, "Unsupported ServiceX returned format"
-            method_to_call = self._format_map[default_format]
-
-            stream = a.args[0]
-            col_names = a.args[1]
-            if method_to_call == "get_data_rootfiles_async":
-                # If we have no column names, then we must be using a dictionary to
-                # set them - so just pass that
-                # directly.
-                assert isinstance(
-                    col_names, (ast.List, ast.Constant, ast.Str)
-                ), f"Programming error - type name not known {type(col_names).__name__}"
-                if isinstance(col_names, ast.List) and len(col_names.elts) == 0:
-                    source = stream
-                else:
-                    source = ast.Call(
-                        func=ast.Name(id="ResultTTree", ctx=ast.Load()),
-                        args=[
-                            stream,
-                            col_names,
-                            ast.Str("treeme"),
-                            ast.Str("junk.root"),
-                        ],
-                    )
-            elif method_to_call == "get_data_parquet_async":
-                source = stream
-                # See #32 for why this is commented out
-                # source = ast.Call(
-                #     func=ast.Name(id='ResultParquet', ctx=ast.Load()),
-                #     args=[stream, col_names, ast.Str('junk.parquet')])
-            else:  # pragma: no cover
-                # This indicates a programming error
-                assert False, f"Do not know how to call {method_to_call}"
-
-        elif top_function == "ResultParquet":
-            # Strip off the Parquet function, do a select if there are arguments for column names
-            source = a.args[0]
-            col_names = cast(ast.List, a.args[1]).elts
-
-            def encode_as_tuple_reference(c_names: List) -> List[ast.AST]:
-                # Encode each column ref as a index into the tuple we are being passed
-                return [
-                    ast.Subscript(
-                        value=ast.Name(id="x", ctx=ast.Load()),
-                        slice=ast.Constant(idx),
-                        ctx=ast.Load(),
-                    )
-                    for idx, _ in enumerate(c_names)
-                ]
-
-            def encode_as_single_reference():
-                # Single reference for a bare (non-col) variable
-                return [
-                    ast.Name(id="x", ctx=ast.Load()),
-                ]
-
-            if len(col_names) > 0:
-                # Add a select on top to set the column names
-                if len(col_names) == 1:
-                    # Determine if they built a tuple or not
-                    values = (
-                        encode_as_tuple_reference(col_names)
-                        if has_tuple(source)
-                        else encode_as_single_reference()
-                    )
-                elif len(col_names) > 1:
-                    values = encode_as_tuple_reference(col_names)
-                else:
-                    assert False, "make sure that type checkers can figure this out"
-
-                d = ast.Dict(keys=col_names, values=values)
-                tup_func = ast.Lambda(
-                    args=ast.arguments(args=[ast.arg(arg="x")]), body=d
-                )
-                c = ast.Call(
-                    func=ast.Name(id="Select", ctx=ast.Load()),
-                    args=[source, tup_func],
-                    keywords=[],
-                )
-                source = c
-
-        return python_ast_to_text_ast(source)
-
-    def as_qastle(self):
-        return self.value()
